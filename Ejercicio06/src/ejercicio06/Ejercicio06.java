ef0a0687cc267105694b062bbf4f30dfcf9a6159
Candiani Santiago <s.candiani@hotmail.com>
2023-01-23 22:17:47 -0300

    Actualizacion ejercicio 06

--- Ejercicio06/src/ejercicio06/Ejercicio06.java
+++ Ejercicio06/src/ejercicio06/Ejercicio06.java
@@ -18,38 +18,7 @@ public class Ejercicio06 {
 
         Servicios serv = new Servicios();
 
-        Scanner leer = new Scanner(System.in).useDelimiter("\n");
-        
-        String exit="";
-
-        do {
-
-            System.out.println("MENU");
-            System.out.println("1- Ingresar Producto");
-            System.out.println("2- Mostrar Producto");
-            System.out.println("3- Cambiar precio");
-            System.out.println("4- Eliminar Producto");
-            System.out.println("5- Exit");
-
-            switch (leer.next()) {
-                case "1":
-                    serv.crearProducto();
-                    break;
-                case "2":
-                    serv.mostrarLista();
-                    break;
-                case "3":
-                    serv.modificarPrecio();
-                    break;
-                case "4":
-                    serv.eliminarProducto();
-                    break;
-                case "5":
-                    System.out.println("Esta seguro que desea salir (Y/N)?");
-                    exit = leer.next();
-            }
-
-        } while (!exit.equalsIgnoreCase("y"));
+        serv.menu();
 
     }//main
 }//class
--- Ejercicio06/src/ejercicio06/servicios/Servicios.java
+++ Ejercicio06/src/ejercicio06/servicios/Servicios.java
@@ -16,38 +16,40 @@ public class Servicios {
     }
 
     public void crearProducto() {
-        String Exit;
+        String exit;
 
         do {
             Producto p1 = new Producto();
 
-            System.out.println("Ingrese el nombre del producto:");
+            System.out.println("* Ingrese el nombre del producto:");
             p1.setNombreProducto(leer.next());
 
-            System.out.println("Ingrese el precio del producto:");
+            System.out.println("* Ingrese el precio del producto:");
             p1.setPrecio(leer.nextDouble());
 
             ListaDeProductos.put(p1.getNombreProducto(), p1.getPrecio());
 
-            System.out.println("Si quiere salir ingrese S");
-            System.out.println("En caso contrario cualquier letra para continuar");
-            Exit = leer.next();
+            System.out.println("Si desea salir al menu principal digite 'S'.");
+            System.out.println("Si desea seguir cargando productos digite cualquier otra letra.");
+            exit = leer.next();
 
-        } while (!Exit.equalsIgnoreCase("S"));
+        } while (!exit.equalsIgnoreCase("S"));
     }//crearProducto
 
     public void mostrarLista() {
+        System.out.println("LISTA DE PRODUCTOS:");
+
         for (Map.Entry<String, Double> entry : ListaDeProductos.entrySet()) {
             String key = entry.getKey();
             Double value = entry.getValue();
 
-            System.out.println("Producto: " + key + "\nPrecio:" + value);
+            System.out.println("#Producto: " + key + "\n#Precio:" + value);
         }
-    }
+    }//mostrarLista
 
     public void eliminarProducto() {
 
-        System.out.println("Que producto desea eliminar?");
+        System.out.println("* Que producto desea eliminar?");
         String elimiar = leer.next();
 
         ListaDeProductos.remove(elimiar);
@@ -55,17 +57,56 @@ public class Servicios {
 
     public void modificarPrecio() {
 
-        System.out.println("A que producto desea cambiar el precio?");
+        System.out.println("* A que producto desea cambiar el precio?");
         String cambiar = leer.next();
 
         for (String aux : ListaDeProductos.keySet()) {
             if (aux.equalsIgnoreCase(cambiar)) {
-                System.out.println("Ingrese el precio nuevo:");
+                System.out.println("* Ingrese el precio nuevo:");
                 ListaDeProductos.replace(aux, leer.nextDouble());
             }
         }
     }
-    
-    
+
+    public void menu() {
+        String exit = "";
+
+        do {
+
+            System.out.println("---------MENU---------");
+            System.out.println("1- Ingresar Producto.");
+            System.out.println("2- Mostrar Producto.");
+            System.out.println("3- Cambiar precio.");
+            System.out.println("4- Eliminar Producto.");
+            System.out.println("5- Exit.");
+            System.out.println("----------------------");
+
+            switch (leer.next()) {
+                case "1":
+                    crearProducto();
+                    break;
+                case "2":
+                    mostrarLista();
+                    break;
+                case "3":
+                    modificarPrecio();
+                    break;
+                case "4":
+                    eliminarProducto();
+                    break;
+                case "5":
+                    System.out.println("Si quiere salir ingrese 'S'.");
+                    System.out.println("En caso contrario cualquier letra para continuar.");
+                    exit = leer.next();
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta, vuelva a intentarlo.");
+                    System.out.println("");
+            }
+
+        } while (!exit.equalsIgnoreCase("S"));
+        System.out.println("¡¡USTED SALIO DEL PROGRAMA CON EXITO!!");
+
+    }
 
 }//Class
